4c7fcffed3f298572c4fcb024e5c0edca3b09aec
Glen <glencbz@gmail.com>
2015-11-21 02:03:49 +0800

    made fancier

--- edimensionWeekMover.user.js
+++ edimensionWeekMover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         eDimension Week Mover
 // @namespace    https://github.com/glencbz/edimensionWeekMover
-// @version      0.6
+// @version      0.7
 // @description  Moves the current course to the top of the
 // @author       Glen Choo
 // @match        http://edimension.sutd.edu.sg/course/view.php?id=*
@@ -17,4 +17,8 @@ var $current = $(".current");
 //Change the background color to whatever you like
 $current.css("background-color", "#D7F0FF");
 var copy = $current.clone();
+copy.css("border", "2px solid #2FB1FF");
+copy.css("border-radius", "4px");
+copy.children(".content").prepend("<h2 class='this-week-title'>This Week</h2>");
+$("h2.this-week-title").css("font-family", 'Helvetica Neue",Helvetica,Arial,sans-serif');
 $("ul.weeks").prepend(copy);
