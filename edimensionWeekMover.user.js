6a476fb81a32382235abca46b1c612bf6ae78505
Glen <glencbz@gmail.com>
2015-11-23 14:56:55 +0800

    added week numbers

--- edimensionWeekMover.user.js
+++ edimensionWeekMover.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         eDimension Week Mover
 // @namespace    https://github.com/glencbz/edimensionWeekMover
-// @version      0.72
-// @description  Moves the current course to the top of the
+// @version      0.75
+// @description  Moves the current course to the top of the page
 // @author       Glen Choo
 // @match        http://edimension.sutd.edu.sg/course/view.php?id=*
 // @grant       GM_addStyle
@@ -15,16 +15,35 @@ var $ = window.jQuery;
 var $current = $(".current");
 
 //Change the background color to whatever you like
-$current.css("background-color", "#D7F0FF");
+var currentBackgroundColor = "#D7F0FF";
+
+var styles = ["<style>h2.week-title{color: black; font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;}</style>"];
+styles.push("<style>.current{background-color:" + currentBackgroundColor + " ;}</style>")
+styles.push("<style>h3.sectionname{color: #505050;}</style>")
+
+$("body").append(styles);
+
 var copy = $current.clone();
 copy.css("border", "2px solid #2FB1FF");
 copy.css("border-radius", "4px");
-copy.children(".content").prepend("<h2 class='this-week-title'>This Week</h2>");
+copy.children(".content").prepend("<h2 class='week-title'>This Week</h2>");
 copy.attr("id", "current-section");
 copy.removeClass("current");
 
-$("ul.weeks").prepend(copy);
-$("h2.this-week-title").css("font-family", 'Helvetica Neue",Helvetica,Arial,sans-serif');
+var $weeks = $("ul.weeks");
+$weeks.prepend(copy);
+
+$weeks.children().each(function(i, li){
+	var $li = $(li);
+	var id = $li.attr("id");
+	if (id.indexOf("section-") === 0 ){
+		var weekNumber = id.substring("section-".length);
+		if (weekNumber == 0)
+			return;
+		$li.children(".content").prepend("<h2 class='week-title'>Week " + weekNumber +"</h2>");
+	}
+});
+
 $(".current").click(function(){
 	console.log($("#current-section").offset().top);
 	$("body").animate({
